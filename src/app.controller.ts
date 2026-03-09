d551db8b5ea3459d8ddcfb0a618a395a30cf2aae
ah-malik <88552973+ah-malik@users.noreply.github.com>
2026-03-10 01:02:46 +0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,20 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  
+  @Post('stripe/webhook')
+  handleStripeWebhook(@Req() req: Request, @Res() res: Response) {
+
+    const event = req.body;
+
+    console.log('Stripe event:', event.type);
+
+    if (event.type === 'payment_intent.succeeded') {
+      console.log('Payment successful');
+    }
+
+    return res.status(200).send('Webhook received');
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
